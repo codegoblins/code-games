a0b80385dfb1a479a9f0890edc7b5d2ebd00b85c
Jeff Sweeney <jeff.j.sweeney@gmail.com>
2016-08-02 18:59:59 -0700

    Add browsersync and serve:dev gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,22 +6,38 @@ import sass from 'gulp-sass';
 import cssnano from 'gulp-cssnano';
 import gulpIf from 'gulp-if';
 import babel from 'gulp-babel';
+import browserSync from 'browser-sync';
 
-gulp.task('serve:dev', ['watch'], () => {
+var _browserSync = browserSync.create();
 
-});
-
-gulp.task('watch', () => {
-	gulp.watch('src/stylesheets/sass/*.scss', ['sass']);
-	gulp.watch('src/js/*.js', ['babel']);
-});
-
-gulp.task('serve', ['build-site'], () => {
+function serveStaticSite() {
 	let server = express();
 
 	server.use(express.static('dist'));
 
 	server.listen(3000);
+}
+
+gulp.task('serve:dev', ['useref:dev'], () => {
+	_browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+		browser: []
+	});
+
+	gulp.start('watch');
+});
+
+gulp.task('browsersync-reload', ['useref:dev'], () => {
+	_browserSync.reload();
+});
+
+gulp.task('serve', ['build-site'], serveStaticSite);
+
+gulp.task('watch', () => {
+	gulp.watch('src/stylesheets/sass/*.scss', ['browsersync-reload']);
+	gulp.watch('src/js/*.js', ['browsersync-reload']);
 });
 
 gulp.task('build-site', ['useref']);
@@ -45,12 +61,16 @@ gulp.task('babel', () => {
 		.pipe(gulp.dest('dist/js/'));
 });
 
+gulp.task('useref:dev', ['sass', 'babel'], () => {
+	gulp.src('src/index.html')
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('useref', ['sass', 'babel'], () => {
 	gulp.src('src/index.html')
 		.pipe(useref())
 		.pipe(gulpIf('*.js', uglify()))
 		.pipe(gulpIf('*.css', cssnano()))
-
 		.pipe(gulp.dest('dist'));
 });
-
